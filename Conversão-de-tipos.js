49f68c8653db07d937e8853bb6fb5028dd65316c
Walter <waltermanoel.pro@gmail.com>
2022-03-29 22:21:46 -0300

    Create Conversão-de-tipos.js

---
+++ Conversão-de-tipos.js
@@ -0,0 +1,20 @@
+// tipo de dado 
+// bootleanos
+
+// conversão implícita
+/*const numero = 456;
+const string = "456";
+
+console.log(numero == string);
+
+console.log(numero + string)*/
+
+// conversão explícita
+
+// Number()
+//String()
+
+const numero = 456;
+const string = "456";
+
+console.log(Number(string) + numero)
