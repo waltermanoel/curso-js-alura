ca3a6aebe21650959cc187fcf601a1e0b7706c15
Walter <waltermanoel.pro@gmail.com>
2022-03-29 21:46:03 -0300

    tipos de variáveis

---
+++ Truthy e falsy.js
@@ -0,0 +1,24 @@
+// boolean
+
+// truthy ou falsy
+
+// 0 = false; 1 = true;
+
+/*console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)*/
+
+// undefined
+// null 
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let string = "Alura";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
---
+++ script.js
@@ -0,0 +1,41 @@
+// var
+
+/*var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+
+console.log(area);
+
+var area;*/
+
+// let 
+
+/*let forma = "retângulo";
+let altura = 3;
+let comprimento = 9;
+
+let area;
+
+if (forma === "retângulo") {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)*/
+
+// const 
+
+const forma = "quadrado";
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if (forma === "quadrado") {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
