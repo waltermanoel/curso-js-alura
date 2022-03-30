93e6bb82293e26acbfc35d18fce6608acf236b6a
Walter <waltermanoel.pro@gmail.com>
2022-03-30 11:38:31 -0300

    Sabendo mais sobre o JS

--- Conversão-de-tipos.js
+++ Conversão-de-tipos.js
@@ -17,4 +17,17 @@ console.log(numero + string)*/
 const numero = 456;
 const string = "456";
 
-console.log(Number(string) + numero)
+//console.log(Number(string) + numero)
+
+var respostaDeTudo = 42
+let idade = 29
+const pi = 3.14
+
+{
+    var respostaDeTudo = 3.14
+    let idade = 42
+    const pi = 29
+    console.log(respostaDeTudo, idade, pi)
+
+}
+console.log(respostaDeTudo, idade, pi)
