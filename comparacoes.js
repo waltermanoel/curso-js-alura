6c37fb934c05ad1459963a16e694e2a41f63b7ae
Walter <waltermanoel.pro@gmail.com>
2022-03-31 20:48:02 -0300

    Comparações

---
+++ comparacoes.js
@@ -0,0 +1,14 @@
+// == (compatação implícita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+// === (comparação explícita)
+
+console.log(numero === texto);
+
+// typeof 
+console.log(typeof numero);
+console.log(typeof texto);
