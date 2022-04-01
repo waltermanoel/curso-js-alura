f7e9e3787cc19e03f8b4855bb46a5e633e914c69
Walter <waltermanoel.pro@gmail.com>
2022-03-31 22:00:02 -0300

    Funções

---
+++ funcoes.js
@@ -0,0 +1,23 @@
+/*let x = "";
+console.log(x);
+x = "oi!";*/
+
+// declarar a função
+// 1) 
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+// execução de função
+imprimeTexto("Hello, World!");
+
+// três formas de escrever funções
+
+// 2)
+function soma() {
+    return 2 + 2;
+}
+
+//console.log(soma());
+
+imprimeTexto(soma());
