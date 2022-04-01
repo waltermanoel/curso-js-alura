bd9e8d658226377707b341aceada19fa58d52aa9
Walter <waltermanoel.pro@gmail.com>
2022-03-31 21:30:17 -0300

    Template-string

---
+++ template String.js
@@ -0,0 +1,7 @@
+const nome = "Walter";
+const idade = 17;
+const cidadeDeNascimento = "Ouro Preto D'Oeste"
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} anos e nasci na cidade de ${cidadeDeNascimento}`;
+
+console.log(apresentacao);
