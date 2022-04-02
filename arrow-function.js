0067a695473ab8083e6794f582d82a8f335e1fb1
Walter <waltermanoel.pro@gmail.com>
2022-04-01 21:38:59 -0300

    Arraw functions

---
+++ arrow-function.js
@@ -0,0 +1,16 @@
+// arraw function
+
+const apresentarArraw = nome => `Meu nome é ${nome}`;
+const soma = (n1, n2) => n1 + n2;
+
+// Array function com + de uma linha
+
+const somaMumerosPequenos = (n1, n2) => {
+    if(n1 || n2 > 10) {
+        return "Somente números de 1 a 9";
+    } else {
+        return n1 + n2;
+    }
+}
+
+
