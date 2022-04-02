82d925b26af97f4d8a8f7bcd234307f3f888ad11
Walter <waltermanoel.pro@gmail.com>
2022-04-01 21:25:54 -0300

    expressão de função

---
+++ explecoes.js
@@ -0,0 +1,22 @@
+// expleção de função 
+
+ /*const soma = function(n1, n2) {
+     return n1 + n2;
+ }*/
+
+ //console.log(soma(1, 1));
+
+ // diferencia principal ??????
+
+ console.log(apresentar())
+
+ function apresentar() {
+     return "Olá";
+ }
+
+console.log(soma(1, 1))
+
+const soma = function(n1, n2) {
+    return n1 + n2;
+}
+ 
