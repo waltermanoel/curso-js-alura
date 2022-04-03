7cb3f2a83960ef25424d919724bb7b6c374fca97
Walter <waltermanoel.pro@gmail.com>
2022-04-03 13:14:01 -0300

    splice-arrays

---
+++ curso-fundamentos-arrays/altualizando-lista.js
@@ -0,0 +1,7 @@
+const listaDeChamada = ["João", "Ana", "Caio", "Lara", "Marjorei", "Leo"];
+console.log(listaDeChamada);
+
+//listaDeChamada.splice(1, 2, "Rodrigo");
+listaDeChamada.splice(2, 0, "Rodrigo");
+
+console.log(listaDeChamada);
