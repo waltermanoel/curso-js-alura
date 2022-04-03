e88171bb08e74df9d6d7b0262df76f70182a966a
Walter <waltermanoel.pro@gmail.com>
2022-04-03 13:34:04 -0300

    Método concat()

---
+++ curso-fundamentos-arrays/juntando-turmas.js
@@ -0,0 +1,6 @@
+const salaDePython = ["Melissa", "Helena", "Rodirgo"];
+const salaDeJavaScript = ["Ju", "Leu", "Raquel"];
+
+const salasUnificadas = salaDePython.concat(salaDeJavaScript);
+
+console.log(salasUnificadas);
