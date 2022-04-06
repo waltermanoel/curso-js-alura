d8a7f961c1bc3ac359e75a0a4596c1c5f1175b4d
Walter <waltermanoel.pro@gmail.com>
2022-04-06 08:41:27 -0300

    Update media-for.js

--- curso-fundamentos-arrays/media-for.js
+++ curso-fundamentos-arrays/media-for.js
@@ -2,11 +2,11 @@ const notas = [10, 6.5, 8, 7.5];
 
 console.log(media(notas));
 
-function media(notas, PF=1) {
+function media(notas, pF=1) {
     let somaDasNotas = 0;
 
     for (let i = 0; i < notas.length; i++) {
         somaDasNotas += notas[i];
     };
-    return (somaDasNotas / notas.length).toFixed(PF);
+    return (somaDasNotas / notas.length).toFixed(pF);
 };
