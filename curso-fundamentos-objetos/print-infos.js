804fe9adb81ef4d42ba2f78b7ba0b6341b634e14
Walter <waltermanoel.pro@gmail.com>
2022-04-09 12:43:55 -0300

    Buscando valores em objetos

---
+++ curso-fundamentos-objetos/print-infos.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "Walter",
+    idade: 17, cpf: "556496466",
+    email: "walter@gmail.com"
+};
+
+console.log(`Nome do cliente: ${cliente.nome}`);
+console.log(`Idade do cliente: ${cliente.idade} anos`);
+console.log(`CPF dp cliente: ${cliente.cpf.substring(0, 3)}`);
