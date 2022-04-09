15375daf1346a82d4e1912b129dcd829b70c8f0b
Walter <waltermanoel.pro@gmail.com>
2022-04-08 21:28:11 -0300

    Primeiros passos com objetos

---
+++ curso-fundamentos-objetos/objetos.js
@@ -0,0 +1,9 @@
+//const listaCpf = ["12345678", "516645496", "487878778"];
+
+//const cliente = ["nome", "André", "idade", 17];
+
+const cliente = {nome: "Walter", idade: 17, cpf: "556496466", email: "walter@gmail.com"};
+
+const nome = cliente.nome;
+
+console.log(nome);
