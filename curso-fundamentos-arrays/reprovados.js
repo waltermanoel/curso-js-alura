d551e974410e656415b7dcbaab63d092fad796d5
Walter <waltermanoel.pro@gmail.com>
2022-04-09 17:40:03 -0300

    Arrays nos objetos

--- curso-fundamentos-arrays/reprovados.js
+++ curso-fundamentos-arrays/reprovados.js
@@ -1,7 +1,7 @@
 const alunos = ["Ana", "Marcos", "Maria", "Mauro"];
 const notas = [7, 4.5, 8, 7.5];
 
-const reprovados = alunos.filter((aluno, indice) => 
+const reprovados = alunos.filter((_, indice) => 
     notas[indice] < 6
 );
 
---
+++ curso-fundamentos-objetos/acessar-telefones.js
@@ -0,0 +1,13 @@
+const cliente = {
+    nome: "Walter",
+    idade: 17,
+    cpf: "556496466",
+    email: "walter@gmail.com"
+};
+
+const fones = ["56549849", "9659595", "88948548", "556545466"];
+
+cliente.fones = fones;
+
+cliente.fones.forEach(element => console.log(element));
+
