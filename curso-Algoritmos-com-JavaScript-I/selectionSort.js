fbcff0ffb03cd2aee259baa280c40ef23f0a194e
Walter <waltermanoel.pro@gmail.com>
2022-04-19 11:58:45 -0300

    Ordenando arrays

---
+++ curso-Algoritmos-com-JavaScript-I/selectionSort.js
@@ -0,0 +1,19 @@
+const livros = require("./listaLivros");
+const menorValor = require("./menorValor");
+
+for(let atual = 0; atual < livros.length - 1; atual++) {
+    let menor = menorValor(livros, atual);
+
+    let livroAtual = livros[atual]; 
+    console.log("Posição atual", atual);
+    console.log(livros[atual]);
+    let livroMenorPreco = livros[menor];
+    console.log(livros[menor]);
+
+    livros[atual] = livroMenorPreco;
+    livros[menor] = livroAtual;
+
+
+};
+
+console.log(livros);
