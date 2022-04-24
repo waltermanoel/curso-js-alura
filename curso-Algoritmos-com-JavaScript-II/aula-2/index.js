4efe4a4a708d3a1843104173fe1ce27dd3769a7b
Walter <waltermanoel.pro@gmail.com>
2022-04-24 20:56:06 -0300

    teste com mergeSort

--- curso-Algoritmos-com-JavaScript-II/aula-2/index.js
+++ curso-Algoritmos-com-JavaScript-II/aula-2/index.js
@@ -1,10 +1,14 @@
 const listaLivros = require("./array");
 
-function mergeSort(array) {
+function mergeSort(array, nivelAninhamento=0) {
+
+    console.log(`Nível de aninhamento: ${nivelAninhamento}`);
+    console.log(array);
+
     if (array.length > 1) {
         const meio = Math.floor(array.length / 2);
-        const parte1 = mergeSort(array.slice(0, meio));
-        const parte2 = mergeSort(array.slice(meio, array.length));
+        const parte1 = mergeSort(array.slice(0, meio), ++nivelAninhamento);
+        const parte2 = mergeSort(array.slice(meio, array.length), ++nivelAninhamento);
 
         array = ordena(parte1, parte2);
     };
