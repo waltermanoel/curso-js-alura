c9ff0e85fbf008de408078898ae0f6e23cae626d
Walter <waltermanoel.pro@gmail.com>
2022-05-02 22:14:50 -0300

    Instalando dependências

--- curso-criando-primeira-biblioteca/index.js
+++ curso-criando-primeira-biblioteca/index.js
@@ -1,1 +1,11 @@
+const chalk = require("chalk");
 
+console.log("Vamos começar!");
+
+const paragrafo = "Texto retornado por uma função";
+
+function texto(string) {
+    return string;
+};
+
+console.log(chalk.blue(texto(paragrafo)));
