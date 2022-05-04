4b9506175c0b4558bf235f7335661a0ff2989a24
Walter <waltermanoel.pro@gmail.com>
2022-05-04 11:03:35 -0300

    async/await

--- curso-criando-primeira-biblioteca/index.js
+++ curso-criando-primeira-biblioteca/index.js
@@ -5,12 +5,22 @@ function tratarErro(erro) {
     throw new Error(chalk.red(erro.code, "não há arquivo no caminho"));
 };
 
-function pegaArquivo(caminhoDoArquivo) {
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = "UTF-8";
+    try {
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.green(texto));
+    } catch(erro) {
+        tratarErro(erro);
+    };
+};
+
+/*function pegaArquivo(caminhoDoArquivo) {
     const encoding = "UTF-8";
     fs.promises.readFile(caminhoDoArquivo, encoding)
     .then((texto) => console.log(chalk.green(texto)))
     .catch((erro) => tratarErro(erro));
-};
+};*/
 
 /*function pegaArquivo(caminhoDoArquivo) {
     const encoding = "UTF-8";
@@ -23,4 +33,22 @@ function pegaArquivo(caminhoDoArquivo) {
     });
 };*/
 
-pegaArquivo("./arquivos/texto1.md");
+/*pegaArquivo("./arquivos/texto.md");
+
+function promessa(bool) {
+    const x = bool;
+    return new Promise((resolve, reject) => {
+      if (!x) {
+        reject(new Error("falha na promessa"));
+      }
+      resolve("sucesso na promessa");
+    });
+   }
+   
+   function exibeResposta(textoResult) {
+    console.log(textoResult);
+   }
+   
+   promessa(true)
+    .then((texto) => exibeResposta(texto))
+   // sucesso na promessa*/
