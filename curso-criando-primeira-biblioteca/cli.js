8632a0e0b89b57e5d21431860b07fec5203d25f4
Walter <waltermanoel.pro@gmail.com>
2022-05-07 21:52:35 -0300

    terminado a lib

--- curso-criando-primeira-biblioteca/cli.js
+++ curso-criando-primeira-biblioteca/cli.js
@@ -7,11 +7,10 @@ const caminho = process.argv;
 async function processaTexto(caminhoDoArquivo) {
     const res = await pegaArquivo(caminhoDoArquivo[2]);
     if (caminho[3] === "validar") {
-        console.log(chalk.yellow("Links validados"), validaURLs(res));
+        console.log(chalk.yellow("Links validados"), await validaURLs(res));
     } else {
         console.log(chalk.yellow("Lista de links"), res);
     };
 };
 
 processaTexto(caminho);
- 
--- curso-criando-primeira-biblioteca/http-validacao.js
+++ curso-criando-primeira-biblioteca/http-validacao.js
@@ -1,7 +1,42 @@
-function validaURLs(arrayDeLinks) {
-    
+const fetch = require("node-fetch");
 
-    return arrayDeLinks;
+function manejaErros(erro) {
+    throw new Error(erro.message);
+};
+
+async function checaStatus(arrayDeURLs) {
+    //promises async await
+    try {
+        const arrayDeStatus = await Promise
+            .all(arrayDeURLs
+                .map(async URL => {
+                    const res = await fetch(URL);
+                    return res.status;
+                }
+                )
+            );
+        return arrayDeStatus;
+    } catch (erro) {
+        manejaErros(erro);
+    };
+};
+
+function geraArrayDeURLs(arrayDeLinks) {
+    return arrayDeLinks
+        .map(objetoLink => Object
+            .values(objetoLink).join()
+        );
+};
+
+async function validaURLs(arrayDeLinks) {
+    const liks = geraArrayDeURLs(arrayDeLinks);
+    const statusLinks = await checaStatus(liks);
+    const arrayObjetosLinks = arrayDeLinks
+        .map((objetoLink, indice) => (
+            { ...objetoLink, status: statusLinks[indice]}
+            )
+            );
+    return arrayObjetosLinks;
 };
 
 module.exports = validaURLs;
