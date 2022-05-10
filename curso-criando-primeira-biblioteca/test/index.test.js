44720374a768aedf1615d82c0752ce3417fc83ed
Walter <waltermanoel.pro@gmail.com>
2022-05-09 21:17:13 -0300

    mais testes

--- curso-criando-primeira-biblioteca/test/index.test.js
+++ curso-criando-primeira-biblioteca/test/index.test.js
@@ -1,5 +1,15 @@
 const pegaArquivo = require("../index");
 
-test('deve ser uma função', () => {
-    expect(typeof pegaArquivo).toBe("function");
-  });
+const arrayResult = [
+    {FileList: "https://developer.mozilla.org/pt-BR/docs/Web/API/FileList"}
+];
+
+describe("pegaArquivo::", () => {
+    it("deve ser uma função", () => {
+        expect(typeof pegaArquivo).toBe("function");
+    });
+    it("deve retorna array com resultado", async () => {
+        const resultado = await pegaArquivo("D:/Acesso rápido/Documentos/Estudos/curso-js-alura/curso-criando-primeira-biblioteca/test/arquivos/texto1.md");
+        expect(resultado).toEqual(arrayResult);
+    });
+});
