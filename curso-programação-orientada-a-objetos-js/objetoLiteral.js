92c5e67197b4ffaf3165f3bae1a79ab0aa3c12bb
Walter <waltermanoel.pro@gmail.com>
2022-05-12 10:57:16 -0300

    criando modelo de objeto

--- curso-programação-orientada-a-objetos-js/objetoLiteral.js
+++ curso-programação-orientada-a-objetos-js/objetoLiteral.js
@@ -1,1 +1,12 @@
+const user = {
+    nome: "Walter",
+    email: "walter@gmail.com",
+    nascimento: "2000/05/01",
+    role: "admin",
+    ativo: true,
+    exibirInfos: function() {
+        console.log(this.nome, this.email, this.role);
+    }
+};
 
+user.exibirInfos();
