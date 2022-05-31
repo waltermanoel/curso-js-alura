c98d1673029338ad84bea912d77cb181335cb882
Walter <waltermanoel.pro@gmail.com>
2022-05-30 21:36:40 -0300

    buscando todos os livros do banco de dados

--- curso-API-Rest-com-Express-e-MongoDB/src/app.js
+++ curso-API-Rest-com-Express-e-MongoDB/src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/dbConnect.js";
+import livros from "./models/Livro.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
@@ -10,18 +11,22 @@ const app = express();
 
 app.use(express.json());
 
-const livros = [
+/*const livros = [
     {id: 1, "titulo": "Senhor dos Aneis"},
     {id: 2, "titulo": "O Hobiit"},
     {id: 3, "titulo": "Árvore do dinheiro"}
-];
+];*/
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node");
 });
 
 app.get("/livros", (req, res) => {
+    livros.find((err, livros) => {
     res.status(200).json(livros);
+    });
+
+    
 });
 
 app.get("/livros/:id", (req, res) => {
