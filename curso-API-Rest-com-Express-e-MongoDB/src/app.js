12e0f0733743b352c6c5948a6aec5039de703576
Walter <waltermanoel.pro@gmail.com>
2022-06-08 22:17:58 -0300

    Terminado a CRUD

--- curso-API-Rest-com-Express-e-MongoDB/src/app.js
+++ curso-API-Rest-com-Express-e-MongoDB/src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import livros from "./models/Livro.js";
 import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
@@ -9,38 +8,7 @@ db.once("open", () => {
 });
 
 const app = express();
-
 app.use(express.json());
-
 routes(app);
-
-/*const livros = [
-    {id: 1, "titulo": "Senhor dos Aneis"},
-    {id: 2, "titulo": "O Hobiit"},
-    {id: 3, "titulo": "Árvore do dinheiro"}
-];*/
-
-app.get("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    const livro = livros[index];
-    res.status(200).json(livro);
-});
-
-app.put("/livros/:id", (req, res) => {
-    const index = buscaLivro(req.params.id);
-    livros[index].titulo = req.body.titulo;
-    res.status(201).send(`livro com id: ${req.params.id} foi atualizado com sucesso`);
-});
-
-app.delete("/livros/:id", (req, res) => {
-    const {id} = req.params;
-    const index = buscaLivro(id);
-    livros.splice(index, 1)
-    res.status(201).send(`livro com id: ${req.params.id} foi deletado com sucesso`);
-});
-
-function buscaLivro(id) {
-    return livros.findIndex(livro => livro.id == id);
-};
 
 export default app; 
