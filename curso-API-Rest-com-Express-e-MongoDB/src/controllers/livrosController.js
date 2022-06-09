1bfc5a8bba603caa18299b1cc458513b6bff8f75
Walter <waltermanoel.pro@gmail.com>
2022-06-08 22:01:56 -0300

    Adicionando busca por id

--- curso-API-Rest-com-Express-e-MongoDB/src/controllers/livrosController.js
+++ curso-API-Rest-com-Express-e-MongoDB/src/controllers/livrosController.js
@@ -8,6 +8,18 @@ class LivroController {
         });
     };
 
+    static listarLivroPorId = (req, res) => {
+        const id = req.params.id;
+
+        livros.findById(id, (err, livros) => {
+            if(err) {
+                res.status(400).send({message: `${err.message} - Id do livro não localizado.`})
+            } else {
+                res.status(200).send(livros);
+            };
+        })
+    };
+
     static cadastrarLivro = (req, res) => {
         let livro = new livros(req.body);
 
@@ -19,6 +31,20 @@ class LivroController {
             };
         });
     };
+
+    static atualizarLivro = (req, res) => {
+        const id = req.params.id;
+
+        livros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err) {
+                res.status(200).send({messagem: "Livro atualizado com sucesso!"});
+            } else {
+                res.status(500).send({messagem: err.message});
+            };
+        });
+    
+    };
+
 };
 
 export default LivroController;
